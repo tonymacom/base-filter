aca6b5ce16c62de56e86e9d95f429c47995bfaeb
tony-Ma-yami <tony.ma@yamibuy.com>
2018-09-19 15:52:45 +0800

    创建base-filter

---
+++ src/main/java/com/tony/unit/base/ResponseData.java
@@ -0,0 +1,15 @@
+package com.tony.unit.base;
+
+import lombok.Data;
+
+/**
+ * Created by MABO on 2017/6/27.
+ */
+@Data
+public class ResponseData {
+
+    public int code = 200;
+
+    public Object data;
+
+}
